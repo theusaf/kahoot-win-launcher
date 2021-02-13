750c9dd00a7daabab172af1da03b25a853b6835c
theusaf <theusafyt@gmail.com>
2021-02-13 08:46:54 -0800

    Base setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const electron = require("electron"),
+  {app, BrowserWindow, ipcMain} = electron;
+
+function createWindow() {
+
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
